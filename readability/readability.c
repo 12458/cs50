6ebcb5bd0df9ceb020d8dea0da3f7b6d2876c916
Sim Shang En <simshangen@gmail.com>
2022-06-07 10:22:30 +0000

    Tue, Jun 7, 2022, 10:22 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -36,7 +36,15 @@ int main(void)
     L = letter_count / ((float) word_count / 100.0);
     S = sentence_count / ((float) word_count / 100.0);
     float index = 0.0588 * L - 0.296 * S - 15.8;
-    printf("Grade %i\n", (int) round(index));
+    if (index < 1){
+        printf("Before Grade 1\n");
+    }
+    else if (index > 16){
+        printf("Grade 16+\n");
+    }
+    else{
+        printf("Grade %i\n", (int) round(index));
+    }
     return 0;
 }
 
