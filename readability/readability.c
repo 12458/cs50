2587a7f8985546a7c95c900ccd6b13fee1178239
Sim Shang En <simshangen@gmail.com>
2022-06-07 09:37:24 +0000

    Tue, Jun 7, 2022, 9:37 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -43,4 +43,8 @@ int get_letter_count(string text){
 
 int get_word_count(string text){
     // any sequence of characters separated by spaces should count as a word
+    // need to find the 'edges' of words
+    // case 1: 1 space -- prev and fwd are non-space chars
+    // case 2: >1 space -- prev are non-space while fwd are space chars
+    // case 3: >1 space -- prev is space
 }
