23a0e7eb146d588bbb80ee9ff0ca617fafb962e6
Sim Shang En <simshangen@gmail.com>
2022-06-18 03:12:09 +0000

    Sat, Jun 18, 2022, 3:12 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,7 +66,14 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    for (int i = 0; i < str)
+    if (strcmp(people[i].name, "David") == 0)
+        {
+            printf("Found %s\n", people[i].number);
+            return 0;
+    }
+
+
     return false;
 }
 
