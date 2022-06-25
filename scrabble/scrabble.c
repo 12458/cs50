8be93b9a989c311672ef6336482408b07bf1a4c0
Sim Shang En <simshangen@gmail.com>
2022-06-25 08:35:22 +0000

    Sat, Jun 25, 2022, 8:35 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,11 +2,13 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int compute_score(string word);
+int compute_score_char(char letter);
 
 int main(void)
 {
@@ -20,12 +22,20 @@ int main(void)
 
     // TODO: Print the winner
     if (score1 == score2){printf("Tie!");}
-    else{printf(score1 > score 2 ? "Player 1 wins!\n" : "Player 2 wins!\n")}
+    else{printf(score1 > score2 ? "Player 1 wins!\n" : "Player 2 wins!\n");}
 }
 
 int compute_score(string word)
 {
-    if !islower(word){return 0;}
-    
+    int cumulative = 0;
+    for (int i = 0; i < strlen(word); i++){
+        cumulative += compute_score_char(word[i]);
+    }
+    return cumulative;
     // TODO: Compute and return score for string
+}
+
+int compute_score_char(char letter){
+    if (!islower(letter)){return 0;}
+    return POINTS[atoi(letter)];
 }
