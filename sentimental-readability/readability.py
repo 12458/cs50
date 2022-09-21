e22bab3ce8177275e3387981c523e5b898ec65ba
Sim Shang En <simshangen@gmail.com>
2022-09-21 10:00:13 +0000

    Wed, Sep 21, 2022, 10:00 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,10 +1,21 @@
 import string
+
+
 text = input('Text: ')
+
 # index = 0.0588 * L - 0.296 * S - 15.8
 # where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
 words = text.split(' ')
 word_count = len(words)
 sentence_count = text.count('.') + text.count('!') + text.count('?')
 letter_count = len([i for i in text if i in string.ascii_letters])
-L = letter_count / ()
+L = letter_count / (word_count / 100)
+S = sentence_count / (word_count / 100)
 index = 0.0588 * L - 0.296 * S - 15.8
+
+if index < 1:
+    print('Before Grade 1')
+if index > 16:
+    print('Grade 16+')
+else:
+    print(f'Grade {round(index)}')
