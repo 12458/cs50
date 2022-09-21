81761b401043c55446cbdf01efa50b9a192da89c
Sim Shang En <simshangen@gmail.com>
2022-09-21 09:22:39 +0000

    Wed, Sep 21, 2022, 9:22 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,5 +1,8 @@
-h = int(input('Height: '))
-while h == None or type(h) != :
-    h = int(input('Height: '))
+while True:
+    try:
+        h = int(input('Height: '))
+        break
+    except:
+        pass
 for i in range(1, h+1):
     print(f'{"#"*i : >{h}}  {"#"*i}')
