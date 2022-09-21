0b7b34cbcf72bebcf7fe03d4948a854317cecca7
Sim Shang En <simshangen@gmail.com>
2022-09-21 09:47:34 +0000

    Wed, Sep 21, 2022, 9:47 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,4 @@
 text = input('Text: ')
+# index = 0.0588 * L - 0.296 * S - 15.8
+# where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
+index = int(round(0.0588 * L - 0.296 * S - 15.8))
