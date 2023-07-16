735d9de6700567de82d019581c86b6737b80e780
Sim Shang En <sim@shangen.org>
2023-07-16 16:12:16 +0800

    Changed distrubution code
    This problem set’s distribution code has recently changed. If you downloaded the distribution code prior to Thursday, June 1, 2023 at 7:00 PM GMT+8, run the following terminal commands in your finance directory to download the latest version of the distribution code.
    
    The app.py code has also changed. You are encouraged to save a local copy of your edits to that file first, because executing the below will replace the app.py file. You can then copy some of your implementation changes (e.g. to the buy and sell functions) back into this file.

--- finance/app.py
+++ finance/app.py
@@ -22,10 +22,6 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
-# Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
-
 
 @app.after_request
 def after_request(response):
--- finance/helpers.py
+++ finance/helpers.py
@@ -1,8 +1,12 @@
-import os
+import csv
+import datetime
+import pytz
 import requests
-import urllib.parse
+import subprocess
+import urllib
+import uuid
 
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, session
 from functools import wraps
 
 
@@ -25,7 +29,7 @@ def login_required(f):
     """
     Decorate routes to require login.
 
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -38,24 +42,34 @@ def login_required(f):
 def lookup(symbol):
     """Look up quote for symbol."""
 
-    # Contact API
-    try:
-        api_key = os.environ.get("API_KEY")
-        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.RequestException:
-        return None
+    # Prepare API request
+    symbol = symbol.upper()
+    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
+    start = end - datetime.timedelta(days=7)
 
-    # Parse response
+    # Yahoo Finance API
+    url = (
+        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
+        f"?period1={int(start.timestamp())}"
+        f"&period2={int(end.timestamp())}"
+        f"&interval=1d&events=history&includeAdjustedClose=true"
+    )
+
+    # Query API
     try:
-        quote = response.json()
+        response = requests.get(url, cookies={"session": str(uuid.uuid4())}, headers={"User-Agent": "python-requests", "Accept": "*/*"})
+        response.raise_for_status()
+
+        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
+        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
+        quotes.reverse()
+        price = round(float(quotes[0]["Adj Close"]), 2)
         return {
-            "name": quote["companyName"],
-            "price": float(quote["latestPrice"]),
-            "symbol": quote["symbol"]
+            "name": symbol,
+            "price": price,
+            "symbol": symbol
         }
-    except (KeyError, TypeError, ValueError):
+    except (requests.RequestException, ValueError, KeyError, IndexError):
         return None
 
 
